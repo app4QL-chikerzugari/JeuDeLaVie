1b313c51a6ac365bc0560941b534ef8abc509357
Amina Zugari <azugari@a-109625.ups.u-psud.fr>
2016-05-09 16:03:25 +0200

    [web] css + js

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
-window.onload = function(nombre, taille) {
+function damier() {
         var canvas = document.getElementById('can');
         var context = canvas.getContext('2d');
         var a = [];
         setInterval(anime, 1000);
         
         function anime() {
-            context.fillStyle = "green";
+
+			var nombre = document.getElementById("nbCase").value;
+			var taille = document.getElementById("tailleCase").value;
+            context.fillStyle = "white";
             context.clearRect(0, 0, canvas.width, canvas.height);
             for (i=0; i<nombre; i++) {
                 for (j=0; j<nombre; j++) {
                     if (Math.random() > 0.9) {
                         a[i*nombre+j] = 1;
-                        context.fillStyle = "green";
+                        context.fillStyle = "white";
                     } else {
                         a[i*nombre+j] = 0;
-                        context.fillStyle = "red";
+                        context.fillStyle = "black";
                     }
                     context.fillRect(taille*i, taille*j, taille, taille);
                 }
             }
         }
     }
+    
+ window.onload = damier;
     
     
     
