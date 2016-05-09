61f47c9d4911143aed28fa576520a8fca37e23ad
Wiame <Wiame@client-172-18-70-89.clients.u-psud.fr>
2016-05-09 16:27:49 +0200

    commit web couleur

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 window.onload = function(nombre, taille) {
+
+
         var canvas = document.getElementById('can');
         var context = canvas.getContext('2d');
         var a = [];
-        setInterval(anime, 1000);
+        var color = document.getElementById('color1');
+	
+	var color2 = document.getElementById('color2');
+	setInterval(anime, 1000);
         
         function anime() {
             context.fillStyle = "green";
             context.clearRect(0, 0, canvas.width, canvas.height);
+	var color = document.getElementById('color1');
+        
+        var color2 = document.getElementById('color2');
+
             for (i=0; i<nombre; i++) {
                 for (j=0; j<nombre; j++) {
                     if (Math.random() > 0.9) {
                         a[i*nombre+j] = 1;
-                        context.fillStyle = "green";
+                        context.fillStyle = color;
                     } else {
                         a[i*nombre+j] = 0;
-                        context.fillStyle = "red";
+                        context.fillStyle = color2;
                     }
                     context.fillRect(taille*i, taille*j, taille, taille);
                 }
