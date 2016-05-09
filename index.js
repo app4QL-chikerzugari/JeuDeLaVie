648c7dfaf705b1899c0b63af3a06eeef2734fa26
Amina Zugari <azugari@a-109625.ups.u-psud.fr>
2016-05-09 15:09:30 +0200

    [JS] Le nombre de cases du damier et la taille sont maintenant paramétrables

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-window.onload = function() {
+window.onload = function(nombre, taille) {
         var canvas = document.getElementById('can');
         var context = canvas.getContext('2d');
         var a = [];
@@ -7,16 +7,16 @@ window.onload = function() {
         function anime() {
             context.fillStyle = "green";
             context.clearRect(0, 0, canvas.width, canvas.height);
-            for (i=0; i<10; i++) {
-                for (j=0; j<10; j++) {
+            for (i=0; i<nombre; i++) {
+                for (j=0; j<nombre; j++) {
                     if (Math.random() > 0.9) {
-                        a[i*10+j] = 1;
+                        a[i*nombre+j] = 1;
                         context.fillStyle = "green";
                     } else {
-                        a[i*10+j] = 0;
+                        a[i*nombre+j] = 0;
                         context.fillStyle = "red";
                     }
-                    context.fillRect(10*i, 10*j, 10, 10);
+                    context.fillRect(taille*i, taille*j, taille, taille);
                 }
             }
         }
